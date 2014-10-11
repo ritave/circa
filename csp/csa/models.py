8bf537e3139e735c1d9c04cf4e700f156d0c00d7
Samuel Brebner <samuel_brebner@brown.edu>
2014-10-11 11:24:58 +0200

    Added template for requests

--- csp/csa/models.py
+++ csp/csa/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from django.db import models  
+  
+class Checkin(models.Model):  
+	FREEWATER = 1
+    PICTURESPOT = 2
+    DANGER = 3
+    FREEWIFI = 4
+    OTHER = 5
 
-# Create your models here.
+    CHECKIN_CHOICES = (
+        (FREEWATER, 'Free Water'),
+        (PICTURESPOT, 'Picture Spot'),
+        (DANGER, 'Danger'),
+        (FREEWIFI, 'Free WiFi'),
+        (OTHER, 'Other')
+    )
+
+    unique_id = models.BigIntegerField()  
+    date = models.DateTimeField(auto_now_add=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    checkin_type = models.PositiveSmallIntegerField(max_length=1,
+                                      choices=CHECKIN_CHOICES)
+    other_description = models.TextField(max_length=160, null=True, blank=True)
+    positive = models.PositiveIntegerField()
+    negative = models.PositiveIntegerField()
