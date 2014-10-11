170f34a909706b609edbe4966074468a08cf9743
Hubert T <ht335965@students.mimuw.edu.pl>
2014-10-11 11:18:02 +0200

    restframework

--- csp/csp/settings.py
+++ csp/csp/settings.py
@@ -36,7 +36,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 )
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGINATE_BY': 10
+}
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
