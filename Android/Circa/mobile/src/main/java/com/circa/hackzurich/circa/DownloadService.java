0caabd599f202e37e8bbe5494b486333ea9421f7
dunix15 <dunix15@gmail.com>
2014-10-11 15:09:04 +0200

    http bug

--- Android/Circa/mobile/src/main/java/com/circa/hackzurich/circa/DownloadService.java
+++ Android/Circa/mobile/src/main/java/com/circa/hackzurich/circa/DownloadService.java
@@ -6,11 +6,17 @@ import android.location.Location;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class DownloadService extends Service {
     public DownloadService() {
     }
+
+    private GPSTracker gps;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -20,12 +26,48 @@ public class DownloadService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("Circa", "download_service_on");
+        gps = new GPSTracker(DownloadService.this);
 
         // TO DO - update PushService.places
         // example
-        Place place = new Place(1, Place.getDateTime(), 1.0, 1.0, 0, 0, 1);
+        /*Place place = new Place(1, Place.getDateTime(), 1.0, 1.0, 0, 0, 1);
         PushService.places.clear();
-        PushService.places.add(place);
+        PushService.places.add(place);*/
+        // check if GPS enabled
+        if (gps.canGetLocation()) {
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+            Log.d("Circa", "lat: " + latitude);
+            Log.d("Circa", "long: " + longitude);
+
+            URL url = null;
+            HttpURLConnection connection = null;
+
+            try {
+                url = new URL("http://students.mimuw.edu.pl:33380/notification/");
+                connection = (HttpURLConnection)url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setDoInput(true);
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                Log.d("Circa", "connected5");
+                String notes = br.readLine();
+                Log.d("Circa", "connected6");
+                String buf = "";
+                while ((buf = br.readLine()) != null) {
+                    Log.d("Circa", "connected7");
+                    notes += buf;
+                }
+                Log.d("Circa", "Result: " + notes);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+
+        } else {
+            Log.d("Circa", "GPS or Network is not enabled");
+        }
         Log.d("Circa", "Num of places in cache: " + PushService.places.size());
 
         return Service.START_NOT_STICKY;
